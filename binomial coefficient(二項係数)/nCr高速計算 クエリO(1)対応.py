a7c779ecc8eda338210f279efc962eea55c45dab
3atnk <shinnikkyu@g.ecc.u-tokyo.ac.jp>
2026-01-13 15:19:36 +0900

    Update nCr高速計算 クエリO(1)対応.py

--- binomial coefficient(二項係数)/nCr高速計算 クエリO(1)対応.py
+++ binomial coefficient(二項係数)/nCr高速計算 クエリO(1)対応.py
@@ -3,15 +3,9 @@
 
 class Combination:
     def __init__(self, max_n, mod=998244353):
-        """
-        階乗とその逆元をO(N)で前計算する。
-        max_n: 計算する最大のn（これ以上のnは計算不可）
-        mod: 素数
-        """
         self.mod = mod
         self.max_n = max_n
         
-        # 配列の確保
         self.fact = [1] * (max_n + 1)      # 階乗 n!
         self.finv = [1] * (max_n + 1)      # 逆元 (n!)^-1
         
@@ -29,12 +23,8 @@ class Combination:
             self.finv[i-1] = (self.finv[i] * i) % mod
 
     def nCk(self, n, k):
-        """
-        nCk を O(1) で求める
-        """
         if n < k or n < 0 or k < 0:
             return 0
-        
         # nCk = n! * (k!)^-1 * ((n-k)!)^-1
         return self.fact[n] * self.finv[k] % self.mod * self.finv[n-k] % self.mod
 
